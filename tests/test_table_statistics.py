531c23f132a45998fc7802e7cd10944289ba2817
Adam Marcus <marcua@marcua.net>
2021-07-03 19:56:52 +0000

    Include up to 100 most popular set values

--- tests/test_table_statistics.py
+++ tests/test_table_statistics.py
@@ -16,7 +16,8 @@ def test_table_statistics():
     statistics = column_statistics(engine, Table('synthetic_data'), {})
     assert {
         Column('id'):
-            [SetValuedStatistics(171), RangeValuedStatistics([1, 58, 115])],
+            [SetValuedStatistics(171, list(range(1, 101))),
+             RangeValuedStatistics([1, 58, 115])],
         Column('same_datetime'):
             [RangeValuedStatistics([
                 '2021-05-05 11:00:00.000000'])],
@@ -31,11 +32,14 @@ def test_table_statistics():
                 '2021-05-05 04:00:00.000000',
                 '2021-05-05 07:00:00.000000'])],
         Column('same_string'):
-            [SetValuedStatistics(1)],
+            [SetValuedStatistics(1, ['hi'])],
         Column('unique_string'):
-            [SetValuedStatistics(171)],
+            [SetValuedStatistics(171,
+                                 sorted([f'{bucket}-{row}'
+                                         for bucket in range(1, 20)
+                                         for row in range(1, 10)])[:100])],
         Column('bucket_unique_string'):
-            [SetValuedStatistics(9)],
+            [SetValuedStatistics(9, [str(x) for x in range(1, 10)])],
         Column('same_float'):
             [RangeValuedStatistics([1.1])],
         Column('unique_float'):
@@ -43,11 +47,14 @@ def test_table_statistics():
         Column('bucket_unique_float'):
             [RangeValuedStatistics([1.0, 4.0, 7.0])],
         Column('same_int'):
-            [SetValuedStatistics(1), RangeValuedStatistics([1])],
+            [SetValuedStatistics(1, [1]), RangeValuedStatistics([1])],
         Column('unique_int'):
-            [SetValuedStatistics(171), RangeValuedStatistics([
-                1001, 7004, 13007])],
+            [SetValuedStatistics(171,
+                                 sorted([bucket * 1000 + row
+                                         for bucket in range(1, 20)
+                                         for row in range(1, 10)])[:100]),
+             RangeValuedStatistics([1001, 7004, 13007])],
         Column('bucket_unique_int'):
-            [SetValuedStatistics(9), RangeValuedStatistics([
-                1, 4, 7])],
+            [SetValuedStatistics(9, list(range(1, 10))),
+             RangeValuedStatistics([1, 4, 7])],
     } == statistics
