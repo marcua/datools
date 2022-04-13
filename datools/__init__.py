4e210c9bb980ab7b86bb12594398f791293c1e9b
Adam Marcus <marcua@marcua.net>
2022-04-13 22:21:37 +0000

    Bump version: 0.1.4 → 0.1.5

--- datools/__init__.py
+++ datools/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Adam Marcus"""
 __email__ = 'marcua@marcua.net'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/marcua/datools',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
