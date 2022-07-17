966851f9e23e69d20e5be9b03bbb7b795da4bbea
Adam Marcus <marcua@marcua.net>
2022-07-17 14:46:39 +0000

    Remove support for Python 3.6

--- setup.py
+++ setup.py
@@ -24,13 +24,12 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Adam Marcus",
     author_email='marcua@marcua.net',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
